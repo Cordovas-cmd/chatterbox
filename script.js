e49f2d19b93329886c035db9c2c1c42edc8f37bd
Samuel Cordova <76268942+Cordovas-cmd@users.noreply.github.com>
2023-02-07 19:51:48 -0600

    added some JS elements to start and print the answer to console

---
+++ script.js
@@ -0,0 +1,24 @@
+const answer = document.getElementById("answerForm")
+
+console.log(answerForm);
+
+function runChatbot() {
+const response = document.getElementById("response");
+
+const answer = document.getElementById("answer").value
+
+response.innerText= "Your name is " + answer;
+
+const question = document.getElementById("question")
+
+question.innerText = "When is your birthday?"
+}
+answerForm.addEventListener("submit", function(event) {
+    
+    // prevent default action, meaning stop submission default behavior
+    event.preventDefault();
+
+runChatbot();
+
+
+})
