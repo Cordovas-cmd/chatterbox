5284bde2a1caab53f63f6c52f79997aec2b9d9c7
Samuel Cordova <76268942+Cordovas-cmd@users.noreply.github.com>
2023-02-10 13:30:22 -0600

    added some javascript styles to toggle the visibility between forms

--- script.js
+++ script.js
@@ -1,24 +1,43 @@
+var questionNumber = 0;
 const answer = document.getElementById("answerForm")
 
 console.log(answerForm);
 
 function runChatbot() {
+    event.preventDefault();
 const response = document.getElementById("response");
-
 const answer = document.getElementById("answer").value
-
-response.innerText= "Your name is " + answer + ".";
-
 const question = document.getElementById("question")
 
-question.innerText = "When is your birthday?"
+if (questionNumber === 0) {
+    response.innerText= "Your name is " + answer + ".";
+    question.innerText = "When is your birthday?";
+
+    questionNumber++;
+
+    document.getElementById("birthdayForm").style.visibility = "visible";
+    document.getElementById("answerForm").style.visibility = "hidden";
+} 
+else {
+console.log(questionNumber)
+response.innerText= "You entered a birthday!! ";
+question.innerText = "What is your favorite Holiday?";
+
+// questionNumber++;
+}
+
 }
 answerForm.addEventListener("submit", function(event) {
     
     // prevent default action, meaning stop submission default behavior
-    event.preventDefault();
 
 runChatbot();
 
 
+})
+
+const birthdayForm = document.getElementById("birthdayForm")
+
+birthdayForm.addEventListener("submit", function(event) {
+    runChatbot();
 })
