ddc551697e48d02f8de1c8403d7272317fce439d
Samuel Cordova <76268942+Cordovas-cmd@users.noreply.github.com>
2023-02-15 23:40:33 -0600

    got difference in dates but it's in millliseconds, need it to be in days

--- script.js
+++ script.js
@@ -1,60 +1,59 @@
 var questionNumber = 0;
 const answer = document.getElementById("answerForm")
+const response = document.getElementById("response");
 
 console.log(answerForm);
 
-function calculateNextBirthday(month, day) {
-    // using JS to create a date object (Get year)
-const birthdayYear = new Date().getFullYear();
-console.log(birthdayYear)
-// get value of selected month using index
-const selectedBirthdayMonthElement = document.getElementById("months")
-const birthdayMonth = selectedBirthdayMonthElement.selectedIndex +1;
-
-console.log(birthdayMonth)
-
-// get value of birthday day
-const birthdayDay = document.getElementById("birthdayDayAnswer").value;
-console.log(birthdayDay)
-
-// new instance of Date based on user input
-const birthdayDate = new Date(birthdayYear + "-" + birthdayMonth + "-"  + birthdayDay)
-console.log(birthdayDate)
+function calculateDaysAway(date) {
+    const howManyDaysAwayIsTheDate = new Date() - date;
+return howManyDaysAwayIsTheDate;
 }
+
+function calculateNextBirthday() {
+    // using JS to create a date object (Get year)
+    const birthdayYear = new Date().getFullYear();
+    // get value of selected month using index
+    const selectedBirthdayMonthElement = document.getElementById("months")
+    const birthdayMonth = selectedBirthdayMonthElement.selectedIndex +1;
+
+
+    // get value of birthday day
+    const birthdayDay = document.getElementById("birthdayDayAnswer").value;
+
+    // new instance of Date based on user input
+    const birthdayDate = new Date(birthdayYear + "-" + birthdayMonth + "-"  + birthdayDay)
+    response.innerText = calculateDaysAway(birthdayDate);
+
+    // calculateDaysAway(birthdayDate);
+}
+
 
 function runChatbot() {
     event.preventDefault();
-const response = document.getElementById("response");
-const answer = document.getElementById("answer").value
-const question = document.getElementById("question")
+    const answer = document.getElementById("answer").value
+    const question = document.getElementById("question")
 
-if (questionNumber === 0) {
+    if (questionNumber === 0) {
     response.innerText= "Your name is " + answer + ".";
     question.innerText = "When is your birthday?";
-
     questionNumber++;
-
     document.getElementById("birthdayForm").style.visibility = "visible";
     document.getElementById("answerForm").style.visibility = "hidden";
-} 
-else {
-
+    } 
+    else {
     calculateNextBirthday()
-console.log(questionNumber)
-response.innerText= "You entered a birthday!! ";
-question.innerText = "What is your favorite Holiday?";
-
-// questionNumber++;
-}
+    console.log(questionNumber)
+    question.innerText = "What is your favorite Holiday?";
+    // questionNumber++;
+    }
 
 }
+
 answerForm.addEventListener("submit", function(event) {
     
     // prevent default action, meaning stop submission default behavior
 
 runChatbot();
-
-
 })
 
 const birthdayForm = document.getElementById("birthdayForm")
