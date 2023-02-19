0e291f5490a7e8e9733df76204b693fdb26be35f
Samuel Cordova <76268942+Cordovas-cmd@users.noreply.github.com>
2023-02-18 23:56:29 -0600

    trying to work out holiday form

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const response = document.getElementById("response");
 console.log(answerForm);
 
 function calculateDaysAway(date) {
-    const differenceInMilliseconds = date - new Date() ;
+    const differenceInMilliseconds = date - new Date();
 
     // 1000 milliseconds in a second, 86400 seconds in a day
     const millisecondsInASecond = 1000;
@@ -16,7 +16,7 @@ function calculateDaysAway(date) {
 
     const secondsInAMinute = 60
 
-    const differenceInMinutes =  differenceInSeconds / secondsInAMinute
+    const differenceInMinutes = differenceInSeconds / secondsInAMinute
 
     const minutesInAnHour = 60;
 
@@ -38,20 +38,20 @@ function calculateNextBirthday() {
     // get value of selected month using index
     const selectedBirthdayMonthElement = document.getElementById("months")
 
-    const birthdayMonth = selectedBirthdayMonthElement.selectedIndex +1;
+    const birthdayMonth = selectedBirthdayMonthElement.selectedIndex + 1;
 
 
     // get value of birthday day
     const birthdayDay = document.getElementById("birthdayDayAnswer").value;
 
-     // new instance of Date based on user input
-    let birthdayDate = new Date(birthdayYear + "-" + birthdayMonth + "-"  + birthdayDay)
+    // new instance of Date based on user input
+    let birthdayDate = new Date(birthdayYear + "-" + birthdayMonth + "-" + birthdayDay)
 
 
     // handle a birthday that already happened
     if (new Date() > birthdayDate) {
         birthdayYear = birthdayYear + 1;
-        birthdayDate = new Date(birthdayYear + "-" + birthdayMonth + "-"  + birthdayDay)
+        birthdayDate = new Date(birthdayYear + "-" + birthdayMonth + "-" + birthdayDay)
     }
 
     response.innerText = "Your birthday is  " + calculateDaysAway(birthdayDate) + " days away!";
@@ -64,30 +64,34 @@ function runChatbot() {
     const question = document.getElementById("question")
 
     if (questionNumber === 0) {
-    response.innerText= "Your name is " + answer + ".";
-    question.innerText = "When is your birthday?";
-    questionNumber++;
-    document.getElementById("birthdayForm").style.visibility = "visible";
-    document.getElementById("answerForm").style.visibility = "hidden";
-    } 
-    else {
-    calculateNextBirthday()
-    console.log(questionNumber)
-    question.innerText = "What is your favorite Holiday?";
-    // questionNumber++;
+        response.innerText = "Your name is " + answer + ".";
+        question.innerText = "When is your birthday?";
+        questionNumber++;
+        document.getElementById("birthdayForm").style.visibility = "visible";
+        document.getElementById("answerForm").style.visibility = "hidden";
     }
-
+    else if (questionNumber === 1) {
+        calculateNextBirthday();
+        console.log(questionNumber)
+        question.innerText = "What is your favorite Holiday?";
+        
+    }
+    else if (questionNumber === 2) {
+        response.innerText = "Your Holiday is X days away."
+    } 
+questionNumber++;
 }
 
-answerForm.addEventListener("submit", function(event) {
-    
-    // prevent default action, meaning stop submission default behavior
-
-runChatbot();
+answerForm.addEventListener("submit", function (event) {
+    runChatbot();
 })
 
 const birthdayForm = document.getElementById("birthdayForm")
+birthdayForm.addEventListener("submit", function (event) {
+    runChatbot();
+})
 
-birthdayForm.addEventListener("submit", function(event) {
+const holidayForm = document.getElementById("holidayForm")
+holidayForm.addEventListener("submit", function(event) {
     runChatbot();
 })
